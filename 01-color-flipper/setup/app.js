f0c84c2f0a3882799cab8c335b213a978181c40f
Gama <gamakusumah@gmail.com>
2022-08-02 22:22:39 +0700

    Membuat function Color Flipper

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,16 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const color = document.getElementsByClassName('color')[0];
+const btnChange = document.getElementById('btn');
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
+
+btnChange.addEventListener('click', function() {
+    let randomNumber = getRandomNumber();
+    // alert(randomNumber);
+
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.innerText = colors[randomNumber];
+})
