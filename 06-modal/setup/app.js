46e804794eeef8ef0ed70977e30a649535576a2c
Gama <gamakusumah@gmail.com>
2022-08-03 20:16:50 +0700

    06-modal/setup js functions added

--- 06-modal/setup/app.js
+++ 06-modal/setup/app.js
@@ -1,4 +1,14 @@
 // select modal-btn,modal-overlay,close-btn
+const btnModal = document.querySelector('.modal-btn');
+const modalOverlay = document.querySelector('.modal-overlay');
+const btnClose = document.querySelector('.close-btn');
+
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
+btnModal.addEventListener('click', function() {
+    modalOverlay.classList.toggle('open-modal');
+})
 // when user clicks close-btn remove .open-modal from modal-overlay
+btnClose.addEventListener('click', function() {
+    modalOverlay.classList.toggle('open-modal');
+})
