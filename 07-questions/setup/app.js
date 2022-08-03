91a08587fba66dff06920f9e72535d5dd9899a7e
Gama <gamakusumah@gmail.com>
2022-08-03 21:49:57 +0700

    07-questions/setup/app.js function fixed

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,11 +1,30 @@
 //using selectors inside the element
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function(question) {
+    // console.log(question);
+    const btn = question.querySelector('.question-btn');
+    // console.log(btn);
+    btn.addEventListener('click', function(item) {
+
+        questions.forEach(function(item) {
+            // console.log(item);
+            if(item !== question) {
+                item.classList.remove('show-text');
+            }
+        })
+        question.classList.toggle('show-text');
+    })
+})
+
+
 // traversing the dom
+// const btns = document.querySelectorAll('.question-btn');
 
-const btns = document.querySelectorAll('.question-btn');
-
-btns.forEach(function(btn) {
-    btn.addEventListener('click', function() {
-        const article = btn.parentElement.parentElement;
-        article.classList.toggle('show-text')
-})
-})
+// btns.forEach(function(btn) {
+//     btn.addEventListener('click', function(e) {
+//         const article = e.currentTarget.parentElement.parentElement;
+//         article.classList.toggle('show-text');
+//         console.log(article);
+// })
+// })
