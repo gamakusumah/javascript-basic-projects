7da4e0a3f5d5cb51b74c15a03d333759fb2f54ef
Gama <gamakusumah@gmail.com>
2022-08-03 21:15:53 +0700

    07-questions/setup js functions added

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,11 @@
 //using selectors inside the element
 // traversing the dom
+
+const btns = document.querySelectorAll('.question-btn');
+
+btns.forEach(function(btn) {
+    btn.addEventListener('click', function() {
+        const article = btn.parentElement.parentElement;
+        article.classList.toggle('show-text')
+})
+})
