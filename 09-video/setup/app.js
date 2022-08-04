403fbe72ab03e1185bc39a1814975682deeb3739
Gama <gamakusumah@gmail.com>
2022-08-04 21:07:19 +0700

    09-video/setup js function added

--- 09-video/setup/app.js
+++ 09-video/setup/app.js
@@ -1,3 +1,21 @@
 // MDN
 // The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
 // The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
+
+const btnSwitch = document.querySelector('.switch-btn');
+const video = document.querySelector('.video-container');
+
+btnSwitch.addEventListener('click', function() {
+    if(btnSwitch.classList.toggle('slide')) {
+        video.pause();
+    } else {
+        video.play();
+    };
+})
+
+// preloader
+const preloader = document.querySelector('.preloader');
+
+window.addEventListener('load', function() {
+    preloader.classList.add('hide-preloader');
+});
